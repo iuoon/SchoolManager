7886ce7543340a26fa9b5980f2b3d8b8a29081df
iuoon <710731936@qq.com>
2019-03-17 13:58:54 +0800

    Initial commit

---
+++ admin/src/main/java/com/linln/admin/core/config/UploadFilePathConfig.java
@@ -0,0 +1,22 @@
+package com.linln.admin.core.config;
+
+import com.linln.admin.core.web.FileUpload;
+import com.linln.core.config.properties.ProjectProperties;
+import com.linln.core.utils.SpringContextUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author wuyz
+ * @date 2018/11/3
+ */
+@Configuration
+public class UploadFilePathConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        ProjectProperties properties = SpringContextUtil.getBean(ProjectProperties.class);
+        registry.addResourceHandler(properties.getStaticPathPattern()).addResourceLocations("file:" + FileUpload.getUploadPath());
+    }
+}
---
+++ devtools/src/test/java/com/linln/devtools/code/utils/TemplateUtilTest.java
@@ -0,0 +1,17 @@
+package com.linln.devtools.code.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wuyz
+ * @date 2018/10/24
+ */
+public class TemplateUtilTest {
+
+    @Test
+    public void getTemplate() {
+        TemplateUtil.getTemplate("F:\\Project\\SpringBoot\\Timo\\devtools\\src\\main\\java\\com\\linln\\devtools\\code\\template\\ControllerTemplate.code", "index");
+    }
+}
